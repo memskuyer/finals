d1a0f54970b7056f0272a1d8c1c452dd64b4777a
mamsky <pasteprosmana189@gmail.com>
2025-01-10 22:27:12 +0700

    fix

--- migrations/20250106120309-create-myproject.js
+++ migrations/20250106120309-create-myproject.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { sequelize } = require("../models");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -44,12 +45,10 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
     });
   },
